d6a2299d14f6d92adc14d72b026763ab126fd998
Julie Dodd <doddresidence@gmail.com>
2024-06-18 16:18:37 +0100

    Added a hello to Helen

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Julie"));
+        System.out.println(sayHelloToSomeone("Helen"));
         System.out.println(sayGoodbye());
     }
 
